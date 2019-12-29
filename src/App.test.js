5b1fffb0e6f7a30eb5a604d7cdebef942005915c
Szymon <szymqwerty@gmail.com>
2019-12-29 13:54:31 +0100

    test huhu działa1

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
 test('2+2=4?', () => {
   expect(2 + 2).toBe(4);
 });
